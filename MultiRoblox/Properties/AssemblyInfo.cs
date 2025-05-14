0eae726a0b2d874c0f40b658e29f0d871457fb47
notr3th <notr3th@gmail.com>
2025-05-14 19:33:30 +0800

    Add files via upload

---
+++ MultiRoblox/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("MultiRoblox")]
+[assembly: AssemblyProduct("MultiRoblox")]
+[assembly: AssemblyCopyright("Copyright (c) 2025 notr3th")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
