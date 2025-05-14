3a1ebb8d2b893a2fc464811c7734b3736d9fe52a
notr3th <notr3th@gmail.com>
2025-05-14 10:49:06 +0800

    Delete RoViewer directory

--- RoViewer/RoViewer/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("RoViewer")]
-[assembly: AssemblyProduct("RoViewer")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
